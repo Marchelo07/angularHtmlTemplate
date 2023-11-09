dec8a6abdfc1f37463a695c4dc4114b511acbdad
MARCHELO\Marchelo <chelo_marcelo_ldu@hotmail.com>
2023-11-08 22:28:53 -0500

    Antes de rutas dinamicas

---
+++ src/app/pages/potafolio/potafolio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-potafolio',
+  templateUrl: './potafolio.component.html',
+  styleUrls: ['./potafolio.component.css']
+})
+export class PotafolioComponent {
+
+}
